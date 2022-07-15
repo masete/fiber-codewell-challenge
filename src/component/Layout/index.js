2e1b51fd8941eea1944f539f8c31574991584bea
masete <nicholasmasete72@gmail.com>
2022-07-15 09:41:32 +0300

    added routing

--- src/component/Layout/index.js
+++ src/component/Layout/index.js
@@ -1,6 +1,14 @@
+import Navbar from '../Navbar';
 import './index.scss';
 
 const Layout = () =>{
+    return (
+        <>
+        <section>
+            <Navbar/>
+        </section>
+        </>
+    )
 
 };
 
